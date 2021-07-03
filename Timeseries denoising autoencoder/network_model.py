e4a670ed8c3abb6ed02fe703dd8653a66a28be0d
Viktor <todosijevicviktor998@gmail.com>
2021-07-03 18:06:02 +0200

    Refactored the Network and made it work like it should

--- Timeseries denoising autoencoder/network_model.py
+++ Timeseries denoising autoencoder/network_model.py
@@ -1,63 +1,74 @@
 import torch.nn as nn
-import torch.nn.functional as F
-
-from utils.sizes import *
+from utils.sizes import Conv1dLayerSizes, TransposeConv1dLayerSizes
 
 
 class Network(nn.Module):
     def __init__(self, signal_len):
         super(Network, self).__init__()
 
-        conv1_sizes = Conv1dLayerSizes(in_ch=1, out_ch=20, kernel=5)
-        pool1_kernel = 2
+        # encoder
+        conv1_sizes = Conv1dLayerSizes(in_len=signal_len,
+                                       in_ch=1,
+                                       out_ch=20,
+                                       kernel=5,
+                                       pool_kernel=2)
 
         self.conv1 = nn.Sequential(
             nn.Conv1d(in_channels=conv1_sizes.in_ch,
                       out_channels=conv1_sizes.out_ch,
                       kernel_size=conv1_sizes.kernel_size),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=pool1_kernel)
+            nn.MaxPool1d(kernel_size=conv1_sizes.pool_kernel)
         )
 
-        conv2_sizes = Conv1dLayerSizes(in_ch=conv1_sizes.out_ch, out_ch=50, kernel=5)
-        pool2_kernel = 2
+        conv2_sizes = Conv1dLayerSizes(in_len=conv1_sizes.out_len,
+                                       in_ch=conv1_sizes.out_ch,
+                                       out_ch=50,
+                                       kernel=5,
+                                       pool_kernel=2)
 
         self.conv2 = nn.Sequential(
             nn.Conv1d(in_channels=conv2_sizes.in_ch,
                       out_channels=conv2_sizes.out_ch,
                       kernel_size=conv2_sizes.kernel_size),
             nn.ReLU(),
-            nn.MaxPool1d(kernel_size=pool2_kernel)
+            nn.MaxPool1d(kernel_size=conv2_sizes.pool_kernel)
         )
 
-        stride = 2
-        out_deconv1 = round(signal_len / 2)
-        # TODO foo for kernel size calc
-        # Lout = (Lin - 1) * stride - 2*padding + dilation*(kernel_size - 1) + output_padding + 1
-        kernel_deconv1 = out_deconv1 - stride * (out_conv2 - 1)
-        kernel_deconv2 = signal_len - stride * (out_deconv1 - 1)
+        # decoder
 
-        self.deconv1 = nn.ConvTranspose1d(in_channels=50, out_channels=35, stride=stride, kernel_size=kernel_deconv1)
-        self.deconv2 = nn.ConvTranspose1d(in_channels=35, out_channels=1, stride=stride, kernel_size=kernel_deconv2)
+        deconv1_sizes = TransposeConv1dLayerSizes(in_len=conv2_sizes.out_len,
+                                                  out_len=round(signal_len / 2),
+                                                  in_ch=conv2_sizes.out_ch,
+                                                  out_ch=35,
+                                                  stride=2)
+
+        self.deconv1 = nn.Sequential(
+            nn.ConvTranspose1d(in_channels=deconv1_sizes.in_ch,
+                               out_channels=deconv1_sizes.out_ch,
+                               stride=deconv1_sizes.stride,
+                               kernel_size=deconv1_sizes.kernel_size),
+            nn.ReLU()
+        )
+
+        deconv2_sizes = TransposeConv1dLayerSizes(in_len=deconv1_sizes.out_len,
+                                                  out_len=signal_len,
+                                                  in_ch=deconv1_sizes.out_ch,
+                                                  out_ch=1,
+                                                  stride=2)
+
+        self.deconv2 = nn.ConvTranspose1d(in_channels=deconv2_sizes.in_ch,
+                                          out_channels=deconv2_sizes.out_ch,
+                                          stride=deconv2_sizes.stride,
+                                          kernel_size=deconv2_sizes.kernel_size)
 
     def forward(self, x):
         # encoder
-        # 1
         x = self.conv1(x)
-        x = F.relu(x)
-        x = self.pool(x)
-
-        # 2
         x = self.conv2(x)
-        x = F.relu(x)
-        x = self.pool(x)
 
         # decoder
-        # 1
         x = self.deconv1(x)
-        x = F.relu(x)
-
-        # 2
         x = self.deconv2(x)
 
         return x
--- utils/sizes.py
+++ utils/sizes.py
@@ -1,22 +1,49 @@
-def calcPoolOutSize(in_len, pool_kernel):
-    out_float = in_len / pool_kernel
-    out = int(out_float)
-
-    assert abs(out - out_float) < 1e-4
-    # TODO throw exception
-
-    return out
-
-
 class Conv1dLayerSizes:
-    def __init__(self, in_ch, out_ch, kernel):
+    def __init__(self, in_len, in_ch, out_ch, kernel, pool_kernel):
+        self.in_len = in_len
+        self.pool_kernel = pool_kernel
         self.in_ch = in_ch
         self.out_ch = out_ch
         self.kernel_size = kernel
 
-    def calcOutputLen(self, in_len, pool_kernel=1):
-        out_len = in_len - self.kernel_size + 1
+        self.out_len = self.calcOutputLen()
+
+    # length of the signal after 1d convolution
+    def calcOutputLen(self):
+        out_len = self.in_len - self.kernel_size + 1
         assert out_len > 0
         # TODO throw exception
 
-        return calcPoolOutSize(out_len, pool_kernel)
+        return self.calcPoolOutSize(out_len)
+
+    # length of the signal after pooling
+    def calcPoolOutSize(self, pool_input_len):
+        out_float = pool_input_len / self.pool_kernel
+        out_len = int(out_float)
+
+        assert abs(out_len - out_float) < 1e-4
+        # TODO throw exception
+
+        return out_len
+
+
+class TransposeConv1dLayerSizes:
+    def __init__(self, in_len, out_len, in_ch, out_ch, stride):
+        self.in_len = in_len
+        self.out_len = out_len
+        self.in_ch = in_ch
+        self.out_ch = out_ch
+        self.stride = stride
+
+        self.kernel_size = self.calcKernelSize()
+
+    def calcKernelSize(self):
+        # Lout = (Lin - 1) * stride - 2*padding + dilation*(kernel_size - 1) + output_padding + 1
+        # so the kernel size is
+
+        kernel_size = self.out_len - self.stride * (self.in_len - 1)
+
+        assert kernel_size > 0
+        # TODO throw exception
+
+        return kernel_size
